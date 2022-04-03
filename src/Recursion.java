d2b58024cafc3bce657af7f923746131f6731c1b
AuPhong <stthu98@gmail.com>
2022-04-03 12:11:35 +0700

    message

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,39 @@
+
+
 public class Recursion {
+    /* Recursion
+    Advantages:
+    - easy to read/write
+    - easy to debug
+
+    Disadvantages:
+    - sometimes slower
+    - uses more memory
+     */
+
+    public static void main(String[] args) {
+        //walk(5);
+        //System.out.println(pow(2,7));;
+        System.out.println(factorial(7));;
+    }
+
+    private static int factorial(int base) {
+        if (base<1) return 1;
+        return base*factorial(base-1);
+    }
+
+    private static int  pow(int base, int exponent) {
+        if (exponent<1) return 1;
+        return base * pow(base, exponent-1);
+    }
+
+    private static void walk(int steps) {
+        if (steps < 1) return;
+        System.out.println("You take a step!");
+        walk(steps-1);
+    }
+
+
+
+
 }
